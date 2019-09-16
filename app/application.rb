d7c8bc92c5f9000fb3257625d72bea961fccecc9
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 02:45:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,8 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    time = 12:00 
-    if  time == 12:00
+    time = '12:00 p.m.' 
+    if  time == '12:00 p.m.'
     resp.write ""
     resp.finish
   end
